924168925f7b0de670cd1e39848cbbaa66c3a377
John Flournoy <john_flournoy@fas.harvard.edu>
2021-03-10 13:44:53 -0500

    more time

--- sbatch_trr.bash
+++ sbatch_trr.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH -J trr
-#SBATCH --time=0-01:00:00
+#SBATCH --time=0-02:00:00
 #SBATCH -n 1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=9G
 #SBATCH -p ncf
 #SBATCH --account=mclaughlin_lab
 # Outputs ----------------------------------
-#SBATCH -o log/%x-%A-%a.out
+#SBATCH -o log/%x-%A_%a.out
 
 source /users/jflournoy/code/R_3.5.1_modules.bash
 
-Rscript --vanilla bicc.R
+srun -c 4 Rscript --vanilla bicc.R
